274597f87724f32af1c3e9a1ddef1be34105909b
Abhinav Kumar <147010966+abhi9-0708@users.noreply.github.com>
2023-11-08 05:06:23 +0530

    Add files via upload

---
+++ OutofBoundExceptio.java
@@ -0,0 +1,24 @@
+public class OutofBoundExceptio {
+    public static void main(String[] args) {
+        int i,j,k=0;
+        int a[]=new int[4];
+        i=8;
+        j=0;
+        try{
+            k=i/j;
+            //when an error occur in try then it directly jump to catch statement
+            //so below lines are not getting executed
+            for(int c=0;c<5;c++){
+                a[c]=c+1;
+            }
+            for(int value:a){
+                System.out.println(value);
+            }
+        }
+        catch(ArithmeticException e){
+            System.out.println("can not divide by zero");
+        }
+
+
+    }
+}
